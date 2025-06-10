fa3f090ec8d385b0afb0b050a38c66e18a139ef1
AzureAD\FrankZhangMiningSkil <frank.zhang.sa.au@gmail.com>
2025-06-10 12:16:47 +1000

    add comments

--- src/helpers/Handlebars/getTitle.js
+++ src/helpers/Handlebars/getTitle.js
@@ -1,3 +1,9 @@
+// getTitle.js is a custom Handlebars helper that extracts the corresponding title field value based on the field key. 
+// It is usually used to dynamically generate titles in components.
+
+// name.replace(/\D/g, ""):   remove all non-numeric characters in the field name, such as "field_01" → "01"
+// obj['title_' + index]:     concatenate into the field name "title_01", find this field from data
+// then return its .value
 module.exports = function (obj,name) {
     var index = name.replace(/\D/g, "");
     return obj['title_' + index].value;
